b21c7a6b8d6ba5cddc342a5b2e288147daf048b7
guyue <549975210@qq.com>
2019-11-05 13:43:10 +0800

    20191105

---
+++ src/main/java/com/hu/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.hu.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author hzq
+ * @description test
+ * @date 2019/11/4
+ */
+@Controller
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        log.info("hello");
+        return "hello";
+    }
+}
